b1ef544f243ea655a09012ad3d818f6944e3ab0d
Antoine Ruffion <Antoine Ruffion>
2016-11-09 17:21:11 -0500

    Les buffs sont fonctionnels

---
+++ Assets/TBS Framework/Scripts/ASTR/AttackBuff.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Assets.TBS_Framework.Scripts.ASTR
+{
+    public class AttackBuff : Buff
+    {
+        public int Duration { get { return Duration; } set { Duration = -1; } }
+
+        public void Apply(Unit unit)
+        {
+            unit.AttackFactor *= 2;
+        }
+
+        public Buff Clone()
+        {
+            return this;
+        }
+
+        public void Undo(Unit unit)
+        {
+            unit.AttackFactor /= 2;
+        }
+    }
+}
---
+++ Assets/TBS Framework/Scripts/ASTR/PowerBuff.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace Assets.TBS_Framework.Scripts.ASTR
+{
+    public class AttackBuffSkill : Skill
+    {
+        public override void Apply(Unit caster, Unit receiver)
+        {
+            AttackBuff buff = new AttackBuff();
+            buff.Apply(receiver);
+            caster.ActionPoints--;
+        }
+
+        public override void Apply(Unit caster, Cell receiver, CellGrid cellGrid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
